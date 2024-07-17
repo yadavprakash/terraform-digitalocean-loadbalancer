3f168dd4a19066ecaf17cb49ab2473065bdb40e1
yadavprakash <yadavprakash.it@gmail.com>
2024-07-17 15:48:05 +0530

    update module

--- _example/complete/example.tf
+++ _example/complete/example.tf
@@ -12,7 +12,7 @@ locals {
 ## VPC module call
 ##------------------------------------------------
 module "vpc" {
-  source      = "git::https://github.com/opsstation/terraform-digitalocean-vpc.git?ref=v1.0.0"
+  source      = "git::https://github.com/yadavprakash/terraform-digitalocean-vpc.git?ref=v1.0.0"
   name        = local.name
   environment = local.environment
   label_order = ["name", "environment"]
@@ -25,7 +25,7 @@ module "vpc" {
 ## droplet module call
 ##-----------------------------------------------------------------------
 module "droplet" {
-  source             = "git::https://github.com/opsstation/terraform-digitalocean-droplet.git?ref=v1.0.0"
+  source             = "git::https://github.com/yadavprakash/terraform-digitalocean-droplet.git?ref=v1.0.0"
   name               = local.name
   environment        = local.environment
   region             = local.region
@@ -103,3 +103,4 @@ module "load-balancer" {
     }
   ]
 }
+
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 ### Labels module callled that will be used for naming and tags.
 ###-----------------------------------------------------------------------------
 module "labels" {
-  source      = "git::https://github.com/opsstation/terraform-digitalocean-labels.git?ref=v1.0.0"
+  source      = "git::https://github.com/yadavprakash/terraform-digitalocean-labels.git?ref=v1.0.0"
   name        = var.name
   environment = var.environment
   label_order = var.label_order
